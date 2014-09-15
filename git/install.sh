9722a732f61f365a2ce2952e8ae734644b7a6116
branrich <richbran08@gmail.com>
2014-09-15 14:41:30 -0400

    Fixing a bug resulting from misuse of `ln`.
    `ln` treats a symlink that points to a directory as the target directory and
    creates a new symlink in that directory instead of replacing the existing
    symlink itself. I had to use the '-h' flag to stop this from happening.

--- git/install.sh
+++ git/install.sh
@@ -1,3 +1,3 @@
 cur_file="${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}"
 cur_dir="$(dirname "${cur_file}")"
-ln -sf "${cur_dir}/git_template" "$HOME/.git_template"
+ln -hsf "${cur_dir}/git_template" "$HOME/.git_template"
