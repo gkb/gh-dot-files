fc9f5c7484503d42fda5a86e04ee97cb34a1c07c
branrich <richbran08@gmail.com>
2014-09-15 14:55:48 -0400

    Making the install script more modular
    It was hard to figure out which part of the script was doing what. Splitting up
    the install script into functions was the logical thing to do.
    
    In the process, I found some mistakes in the structure of the script related to
    the creation of the 'install_dir'. Next step would be to fix that.

---
+++ helpers.sh
@@ -0,0 +1,81 @@
+function die {
+        echo "$@" 1>&2 ; exit 1;
+}
+
+function abort_if_file_exists_not_dir {
+        if [[ "$#" != 1 ]]; then
+                die "Invalid number of arguments supplied";
+        fi
+        if [[ ( -a "$1" ) && ( ! -d "$1" ) ]]; then
+                die "A file by the name of $1 exists and is not a directory";
+        fi
+}
+
+function configure_debug {
+        [[ ! -z "$DEBUG" ]] && set -x
+}
+
+function create_install_dir {
+        install_dir="$HOME/.dot-file-collection"
+        abort_if_file_exists_not_dir "$install_dir"
+        mkdir -p "$install_dir";
+}
+
+function set_bash_options {
+        # Recognize dot files correctly
+        shopt -s dotglob
+        shopt -s nullglob
+}
+
+function create_symlinks {
+        for file in $(find -H $install_dir -name '*.symlink') ; do
+                bname=$(basename $file)
+                ln -sf "$file" "$HOME/.${bname%.*}"
+        done
+}
+
+function _sym_link_os_specific_helper {
+        local sys_specific_filename=".sys_specific"
+        local os_specific_dir="${install_dir}/os_specific"
+
+        if [[ -a "${os_specific_dir}/$1" ]] ; then
+                ln -sf "${os_specific_dir}/$1" \
+                        "${HOME}/${sys_specific_filename}";
+        fi
+}
+
+# Symlink the os-specific profile
+function sym_link_os_specific {
+        case "$(uname -s)" in
+        Darwin)
+                _sym_link_os_specific_helper "osx_profile"
+                ;;
+        *)
+                # Assume that it's a unix box by default
+                _sym_link_os_specific_helper "unix_profile"
+                ;;
+        esac
+}
+
+# Run all install files in sub-directories
+function run_install_scripts {
+        # Exclude current file although it's named install.sh as well
+        # Use the -H option in `find` to follow the symlink that is $install_dir
+        for file in $(find -H $install_dir -mindepth 2 -name 'install.sh') ; do
+                bash "$file";
+        done
+}
+
+# Install all bin files in $bin_dir
+function install_bin_files {
+        bin_files=(**/bin/*)
+        bin_dir="$HOME/.bin"
+        abort_if_file_exists_not_dir "$bin_dir"
+        mkdir -p "$bin_dir";
+        # Use the -H option in `find` to follow the symlink that is $install_dir
+        for file in "${bin_files[@]}"; do
+                if [[ -f "$file" ]]; then
+                        cp "$file" "$HOME/.bin";
+                fi;
+        done
+}
--- install.sh
+++ install.sh
@@ -1,65 +1,13 @@
 #!/bin/bash
-function die {
-        echo "$@" 1>&2 ; exit 1;
-}
+source ./helpers.sh
 
-function abort_if_file_exists_not_dir {
-        if [[ "$#" != 1 ]]; then
-                die "Invalid number of arguments supplied";
-        fi
-        if [[ ( -a "$1" ) && ( ! -d "$1" ) ]]; then
-                die "A file by the name of $1 exists and is not a directory";
-        fi
-}
+configure_debug
 
-[[ ! -z "$DEBUG" ]] && set -x
+set_bash_options
 
-install_dir="$HOME/.dot-file-collection"
-abort_if_file_exists_not_dir "$install_dir"
-mkdir -p "$install_dir";
+create_install_dir
+create_symlinks
 
-# Recognize dot files correctly
-shopt -s dotglob
-shopt -s nullglob
-
-for file in $(find -H $install_dir -name '*.symlink') ; do
-        bname=$(basename $file)
-        ln -sf "$file" "$HOME/.${bname%.*}"
-done
-
-sym_link_os_specific() {
-        local sys_specific_filename=".sys_specific"
-        local os_specific_dir="${install_dir}/os_specific"
-
-        if [[ -a "${os_specific_dir}/$1" ]] ; then
-                ln -sf "${os_specific_dir}/$1" \
-                        "${HOME}/${sys_specific_filename}";
-        fi
-}
-
-# Symlink the os-specific profile
-case "$(uname -s)" in
-Darwin)
-        sym_link_os_specific "osx_profile"
-        ;;
-*)
-        # Assume that it's a unix box by default
-        sym_link_os_specific "unix_profile"
-        ;;
-esac
-
-# Run all install files in sub-directories
-# Exclude current file although it's named install.sh as well
-for file in $(find -H $install_dir -mindepth 2 -name 'install.sh') ; do
-        bash "$file";
-done
-
-bin_files=(**/bin/*)
-bin_dir="$HOME/.bin"
-abort_if_file_exists_not_dir "$bin_dir"
-mkdir -p "$bin_dir";
-for file in "${bin_files[@]}"; do
-        if [[ -f "$file" ]]; then
-                cp "$file" "$HOME/.bin";
-        fi;
-done
+sym_link_os_specific
+run_install_scripts
+install_bin_files
