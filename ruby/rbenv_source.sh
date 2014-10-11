8ef422599e00438b3c5f7e035024915e8bdfd57d
branrich <richbran08@gmail.com>
2014-10-11 00:01:12 -0700

    Including the rbenv completions script in install
    Bash started complaining on a new OS X setup and alerted me to the
    rather manual way that I'd set this up on my existing machine.

--- ruby/rbenv_source.sh
+++ ruby/rbenv_source.sh
@@ -1,6 +1,13 @@
 # vim: ft=sh ts=2 sts=2 sw=2 expandtab
 # Provide for tab completion in rbenv
-source ~/.rbenv/completions/rbenv.bash
+export rbenv_completion_dir="$HOME/.rbenv/completions"
+export rbenv_completion_file="${rbenv_completion_dir}/rbenv.bash"
+export github_url_completion_file="https://raw.githubusercontent.com/sstephenson/rbenv/master/completions/rbenv.bash"
+if [[ ! -f "$rbenv_completion_file" ]]; then
+  mkdir -p "$rbenv_completion_dir"
+  (cd "$rbenv_completion_dir"; curl -O "$github_url_completion_file");
+fi
+source "$rbenv_completion_file"
 
 # Adding another function for rbenv to work smoothly
 rbenv() {
