749043976ee07ccdb646195292c219b55b21654b
branrich <richbran08@gmail.com>
2014-10-11 00:37:02 -0700

    Including a gitignore file as part of install.
    This global file was necessary to make git based workflows easier, in
    particular on OS X.

--- git/install.sh
+++ git/install.sh
@@ -1,3 +1,15 @@
 cur_file="${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}"
 cur_dir="$(dirname "${cur_file}")"
-ln -hsf "${cur_dir}/git_template" "$HOME/.git_template"
+
+function create_git_template() {
+        ln -hsf "${cur_dir}/git_template" "$HOME/.git_template"
+}
+
+function create_global_gitignore() {
+        git_config_dir="$HOME/.config/git"
+        mkdir -p "${git_config_dir}"
+        ln -sf "$cur_dir/gitignore/ignore" "$git_config_dir"
+}
+
+create_git_template
+create_global_gitignore
