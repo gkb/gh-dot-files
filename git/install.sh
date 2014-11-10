5b1bc287571e3ad87193049f91c77572273db8a4
branrich <richbran08@gmail.com>
2014-11-10 07:41:55 -0800

    Getting a POSIX compliant ln command in script
    The '-n' option in the ln command is POSIX compliant while the '-h'
    option is not. This was a really simple fix.

--- git/install.sh
+++ git/install.sh
@@ -1,3 +1,3 @@
 cur_file="${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}"
 cur_dir="$(dirname "${cur_file}")"
-ln -hsf "${cur_dir}/git_template" "$HOME/.git_template"
+ln -nsf "${cur_dir}/git_template" "$HOME/.git_template"
