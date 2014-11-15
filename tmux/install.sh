6fe395edd408f8f3ad354921729f29e9f78215ea
Gaju Krishna <gkrishna@palantir.com>
2014-11-15 11:22:10 -0800

    Provide automated tpm install for tmux

---
+++ tmux/install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+tpm_plugin_dir="${HOME}/.tmux/plugins"
+tpm_install_dir="${tpm_plugin_dir}/tpm"
+tpm_git_url="https://github.com/tmux-plugins/tpm"
+
+function get_tpm {
+    mkdir -p "${tpm_plugin_dir}"
+    if [[ ! -d "${tpm_install_dir}" ]]; then
+        git clone "${tpm_git_url}" "$tpm_install_dir"
+    fi
+}
+
+function load_tpm_plugins {
+    local install_script_relpath="scripts/install_plugins.sh"
+    # Need a tmux session active to prevent the script from throwing an error
+    # when it can't find a session where it can display a message
+    local hacky_tmux_session_name="test"
+    local shell_command="${tpm_install_dir}/${install_script_relpath}"
+    tmux new-session -s "${hacky_tmux_session_name}" -d "${shell_command}"
+    tmux kill-session -t "${hacky_tmux_session_name}"
+}
+
+get_tpm
+load_tpm_plugins
